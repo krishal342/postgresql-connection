dd8b03f616392d53f7d494a5fe8c0d24d023adf2
krishal <krishal342@gmail.com>
2026-01-29 09:36:22 +0545

    crud operation

--- controllers/postgresql.controllers.js
+++ controllers/postgresql.controllers.js
@@ -1,12 +1,14 @@
 import { prisma } from "../lib/prisma.js";
 
+
+// create operation
 export const createController = async (req, res, next) => {
     try{
         const { name, email } = req.body;
         const user = await prisma.user.create({
             data: {
                 name,
-                email,
+                email:email.toLowerCase(),
             }
         });
         res.status(201).json(user);
@@ -15,6 +17,7 @@ export const createController = async (req, res, next) => {
     }
 };
 
+// read operation
 export const getAllUserController = async (req, res,next) => {
     try{
         const users = await prisma.user.findMany();
@@ -37,3 +40,42 @@ export const getUserByIdController = async (req, res, next) => {
         next(err);
     }
 };
+
+// update operation
+export const updateUserController = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const { name, email } = req.body;
+        const user = await prisma.user.findUnique({
+            where:{
+                id
+            }
+        });
+        user.name = name || user.name;
+        user.email = email ? email.toLowerCase() : user.email;
+        await prisma.user.update({
+            where: {
+                id
+            },
+            data: user
+        });
+        res.status(200).json(user);
+    }catch(err){
+        next(err);
+    }
+}
+
+// delete operation
+export const deleteUserController = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        await prisma.user.delete({
+            where:{
+                id,
+            }
+        });
+        res.status(200).json({message: 'User deleted successfully'});
+    }catch(err){
+        next(err);
+    }
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import config from './config/config.js';
 import errorMiddleware from './middleware/error.middleware.js';
 
-import {createController, getAllUserController, getUserByIdController} from './controllers/postgresql.controllers.js';
+import {createController, getAllUserController, getUserByIdController, updateUserController, deleteUserController} from './controllers/postgresql.controllers.js';
 
 const app = express();  
 
@@ -19,6 +19,8 @@ app.get('/', (req, res) => {
 app.post('/users', createController);
 app.get('/users', getAllUserController);
 app.get('/users/:id', getUserByIdController);
+app.put('/users/:id', updateUserController);
+app.delete('/users/:id', deleteUserController);
 
 //erro middleware
 app.use(errorMiddleware);
